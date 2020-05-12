7fa272098d5350deeb1dac852ddaba8286359269
AbuHuraira537 <abouhuraira967@gmail.com>
2020-05-12 15:22:00 +0500

    Add project files.

---
+++ ThetaOnlineStoree/Models/ThetaOnlineStoreContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ThetaOnlineStoree.Models
+{
+    public partial class ThetaOnlineStoreContext : DbContext
+    {
+        public ThetaOnlineStoreContext()
+        {
+        }
+
+        public ThetaOnlineStoreContext(DbContextOptions<ThetaOnlineStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<SystemUser> SystemUser { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=HURAIRASPC\\SQLEXPRESS;Database=ThetaOnlineStore;User ID=sa; Password=bsef17m537;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SystemUser>(entity =>
+            {
+                entity.Property(e => e.CreatedBy)
+                    .HasColumnName("Created_By")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnName("Created_Date")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.DisplayName)
+                    .HasColumnName("Display_Name")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.Mobile).HasMaxLength(50);
+
+                entity.Property(e => e.ModifiedBy)
+                    .HasColumnName("Modified_By")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.ModifiedDate)
+                    .HasColumnName("Modified_Date")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.Password).HasMaxLength(50);
+
+                entity.Property(e => e.Role).HasMaxLength(50);
+
+                entity.Property(e => e.Status).HasMaxLength(50);
+
+                entity.Property(e => e.UserName)
+                    .HasColumnName("User_Name")
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
